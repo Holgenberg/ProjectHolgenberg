1d78c8eaeb840f58af5c30cabf7665f0903bbea2
Holgenberg <alexeiholgate1@gmail.com>
2019-09-22 16:49:15 +0100

    Set Background Image With Inline Styles And Folder

--- ProjectHolgenberg/ClientApp/src/components/Home.js
+++ ProjectHolgenberg/ClientApp/src/components/Home.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './Home.css'
+import desktopImage from '../images/home-background.jpg';
 
 export class Home extends Component {
     displayName = Home.name
 
     render() {
         return (
-            <div className="Home"></div>
+            <div className="Home" style={{ backgroundImage: `url(${desktopImage})` }}></div>
         );
     }
 }
