b34ad6159d837721e251bab38a7bc8efdef4cb9b
Holgenberg <alexeiholgate1@gmail.com>
2019-09-28 15:05:55 +0100

    First Addition Of Navbar

--- ProjectHolgenberg/ClientApp/src/components/Home.js
+++ ProjectHolgenberg/ClientApp/src/components/Home.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
+import { render } from 'react-dom';
 import './Home.css'
 import desktopImage from '../images/background.jpg';
+import CheeseburgerMenu from 'cheeseburger-menu'
+import HamburgerMenu from 'react-hamburger-menu'
+import MenuContent from './menuContent'
 
 export class Home extends Component {
     displayName = Home.name
 
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            menuOpen: false,
+        }
+    }
+
+    openMenu() {
+        this.setState({ menuOpen: true })
+    }
+
+    closeMenu() {
+        this.setState({ menuOpen: false })
+    }
+
     render() {
         return (
             <div className="Home" style={{ backgroundImage: `url(${desktopImage})` }}>
+                <CheeseburgerMenu
+                    isOpen={this.state.menuOpen}
+                    closeCallback={this.closeMenu.bind(this)}>
+                    <MenuContent closeCallback={this.closeMenu.bind(this)} />
+                </CheeseburgerMenu>
+
+                <HamburgerMenu
+                    isOpen={this.state.menuOpen}
+                    menuClicked={this.openMenu.bind(this)}                    
+                    width={32}
+                    height={24}
+                    strokeWidth={3}
+                    rotate={0}
+                    color='white'
+                    borderRadius={0}
+                    animationDuration={0.5}
+                />
+
                 <link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet"></link>
                 <div className="Home-content">
                     <h1>Alexei Holgate</h1>
---
+++ ProjectHolgenberg/ClientApp/src/components/menuContent.js
@@ -0,0 +1,38 @@
+﻿import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './menuContent.css'
+
+class MenuContent extends Component {
+  constructor(props) {
+    super(props)
+
+    this.items = []
+    for (let i=1; i<=5; i++) {
+      this.items.push(i)
+    }
+  }
+
+  render() {
+    return (
+      <div className="menu">
+        {this.items.map(i => <div className="menu-item" key={i}>
+          <a
+            href="https://github.com/Middlerun/cheeseburger-menu"
+            onClick={this.props.closeCallback}
+            target="_blank">
+            Menu item {i}
+          </a>
+        </div>)}
+
+        <p className="hint">Click outside the menu to close it, or swipe it closed on touch device</p>
+      </div>
+    )
+  }
+}
+
+MenuContent.propTypes = {
+    closeCallback: PropTypes.func.isRequired
+}
+
+export default MenuContent
