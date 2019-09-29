1942dc33a3500802113e9de1a6221a551531ef85
Holgenberg <alexeiholgate1@gmail.com>
2019-09-29 14:50:35 +0100

    Removed Constructor and Creation Of Duplicate Menu Items

--- ProjectHolgenberg/ClientApp/src/components/NavigationMenuContent.js
+++ ProjectHolgenberg/ClientApp/src/components/NavigationMenuContent.js
@@ -6,24 +6,19 @@ import './NavigationMenuContent.css'
 class NavigationMenuContent extends Component {
   constructor(props) {
     super(props)
-
-    this.items = []
-    for (let i=1; i<=5; i++) {
-      this.items.push(i)
-    }
   }
 
   render() {
     return (
       <div className="menu">
-        {this.items.map(i => <div className="menu-item" key={i}>
+        <div className="menu-item">
           <a
             href="https://github.com/Middlerun/cheeseburger-menu"
             onClick={this.props.closeCallback}
             target="_blank">
-            Menu item {i}
+            Home
           </a>
-        </div>)}
+        </div>
 
         <p className="hint">Click outside the menu to close it, or swipe it closed on touch device</p>
       </div>
