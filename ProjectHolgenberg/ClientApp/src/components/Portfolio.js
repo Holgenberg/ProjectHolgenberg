08e5070456d5dba12ea4829d5d814ba05996a1f9
Holgenberg <alexeiholgate1@gmail.com>
2019-10-07 19:46:08 +0100

    Added Portfolio Component

--- ProjectHolgenberg/ClientApp/src/components/Portfolio.js
+++ ProjectHolgenberg/ClientApp/src/components/Portfolio.js
@@ -1,1 +1,52 @@
-﻿
+﻿import React, { Component } from 'react'
+import './HamburgerMenu.css'
+import CheeseburgerMenu from 'cheeseburger-menu'
+import HamburgerMenu from 'react-hamburger-menu'
+import NavigationMenuContent from './NavigationMenuContent'
+
+export class Portfolio extends Component {
+    displayName = Portfolio.name
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            menuOpen: false,
+        }
+    }
+
+    openMenu() {
+        this.setState({ menuOpen: true })
+    }
+
+    closeMenu() {
+        this.setState({ menuOpen: false })
+    }
+
+    render() {
+        return (
+            <div>
+                <link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet"></link>
+                <CheeseburgerMenu
+                    isOpen={this.state.menuOpen}
+                    closeCallback={this.closeMenu.bind(this)}>
+                    <NavigationMenuContent closeCallback={this.closeMenu.bind(this)} />
+                </CheeseburgerMenu>
+
+                <div className="Hamburger-menu-button-placement">
+                    <HamburgerMenu
+                        isOpen={this.state.menuOpen}
+                        menuClicked={this.openMenu.bind(this)}
+                        width={32}
+                        height={24}
+                        strokeWidth={3}
+                        rotate={0}
+                        color='black'
+                        borderRadius={0}
+                        animationDuration={0.5}
+                    />
+                </div>
+            </div>
+        )
+    }
+}
