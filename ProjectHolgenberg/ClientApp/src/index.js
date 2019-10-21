8831c8cbe2aad4ee1dd51cbf8c8bad4c509fb97b
Holgenberg <alexeiholgate1@gmail.com>
2019-10-21 19:38:04 +0100

    Resized Font

--- ProjectHolgenberg/ClientApp/src/index.js
+++ ProjectHolgenberg/ClientApp/src/index.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 import React from 'react';
