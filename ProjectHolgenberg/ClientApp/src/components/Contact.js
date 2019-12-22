bf5f1f3aff17e395c06796c9e4868c998ef49079
Holgenberg <alexeiholgate1@gmail.com>
2019-12-22 16:47:35 +0000

    Completed Contact Page

--- ProjectHolgenberg/ClientApp/src/components/Contact.js
+++ ProjectHolgenberg/ClientApp/src/components/Contact.js
@@ -49,6 +49,14 @@ export class Contact extends Component {
                     />
                 </div>
 
+                <div className="Contact-content">
+                    <h3>alexeiholgate1@gmail.com</h3>
+                    <p>mob: +447506586992</p>
+                    <h3>Special thanks to:</h3>
+                    <p>Emma's View Photography</p>
+                    <p>middlerun</p>
+                </div>
+
                 <SocialFollow />
             </div>
         )
