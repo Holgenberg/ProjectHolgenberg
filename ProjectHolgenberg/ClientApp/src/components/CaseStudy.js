6a4e64e2544112403cbc3f4524ecaa0bd92cc682
Alexei Holgate <alexeiholgate1@gmail.com>
2023-09-24 09:15:37 +0100

    Added boilerplate for Case Studies

--- ProjectHolgenberg/ClientApp/src/components/CaseStudy.js
+++ ProjectHolgenberg/ClientApp/src/components/CaseStudy.js
@@ -7,9 +7,21 @@ const CaseStudy = ({ caseStudyImage }) => {
     return (
         <div>
             <div className="topRegionStyle" />
-            <div className="caseStudyStyle">
-                <Image src={caseStudyImage} className="imageStyle" />
+            <div className="caseStudyContainer">
+                <div className="imageContainer">
+                    <Image src={caseStudyImage} className="imageStyle" />
+                </div>
+                <div className="textContainer">
+                    <h2>Title</h2>
+                    <p>Sentence goes here.</p>
+                    <ul>
+                        <li>Bullet point 1</li>
+                        <li>Bullet point 2</li>
+                        <li>Bullet point 3</li>
+                    </ul>
+                </div>
             </div>
+
         </div>
     );
 };
